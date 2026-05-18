b0c7891883e41ab0e29e5418bb796cc12d9adc01
ooweaJ <dbwidn2@naver.com>
2026-05-18 17:37:10 +0900

    add : 기술문서 , 데이터 아이템

--- Assets/_dev/Script/ServerConfig.cs
+++ Assets/_dev/Script/ServerConfig.cs
@@ -3,7 +3,7 @@ public static class ServerConfig
     // 로컬 WiFi 테스트 시 PC의 로컬 IP로 변경 (ipconfig로 확인)
     // 예: "192.168.0.10"
     // 배포 시: 실제 서버 공인 IP로 교체
-    public const string GameServerIP = "192.168.3.11";
+    public const string GameServerIP = "192.168.1.39";
 
     public const ushort LobbyPort  = 7777;
     public const ushort BattlePortBase = 7778;
